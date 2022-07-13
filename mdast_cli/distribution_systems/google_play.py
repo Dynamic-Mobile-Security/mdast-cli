356ddf654914a75c3b570f8d30ce8800a0ce4c7d
Nikita Pinaev <nikitapinaev1@gmail.com>
2022-07-13 16:53:03 +0300

    add splits, new emulator x86

--- mdast_cli/distribution_systems/google_play.py
+++ mdast_cli/distribution_systems/google_play.py
@@ -28,29 +28,61 @@ def google_play_download(package_name,
             download_path = 'downloaded_apps'
 
             downloaded_file, app_details = gp_api.download(package_name)
-
-            if file_name is None:
-                file_name = package_name
-
             app_version = app_details.get('versionString')
-            path_to_file = f'{download_path}/{file_name}-v{app_version}.apk'
-            Log.info('Google Play - Successfully logged in Play Store')
-            Log.info(f'Google Play - Downloading {package_name} apk to {path_to_file}')
 
-            if not os.path.exists(download_path):
-                os.mkdir(download_path)
-                Log.info(f'Google Play - Creating directory {download_path} for downloading app from Google Play Store')
+            if not downloaded_file['splits']:
+                if file_name is None:
+                    file_name = package_name
 
-            with open(path_to_file, 'wb') as file:
-                for chunk in downloaded_file.get('file').get('data'):
-                    file.write(chunk)
+                path_to_file = f'{download_path}/{file_name}-v{app_version}.apk'
+                Log.info('Google Play - Successfully logged in Play Store')
+                Log.info(f'Google Play - Downloading {package_name} apk to {path_to_file}')
 
-            if os.path.exists(path_to_file):
-                Log.info('Google Play - Application successfully downloaded!')
+                if not os.path.exists(download_path):
+                    os.mkdir(download_path)
+                    Log.info(f'Google Play - Creating directory {download_path} for downloading app from Google Play Store')
+
+                with open(path_to_file, 'wb') as file:
+                    for chunk in downloaded_file.get('file').get('data'):
+                        file.write(chunk)
+
+                if os.path.exists(path_to_file):
+                    Log.info('Google Play - Application successfully downloaded!')
+                else:
+                    Log.info('Google Play - Failed to download application. '
+                             'Seems like something is wrong with your file path or app file is broken')
+                    sys.exit(4)
             else:
-                Log.info('Google Play - Failed to download application. '
-                         'Seems like something is wrong with your file path or app file is broken')
-                sys.exit(4)
+                path_to_file = f'{download_path}/{package_name}-v{app_version}'
+                Log.info('Google Play - Successfully logged in Play Store')
+                Log.info(f'Google Play - Downloading {package_name} app with split to {path_to_file}')
+
+                if not os.path.exists(download_path):
+                    os.mkdir(download_path)
+                    Log.info(
+                        f'Google Play - Creating directory {download_path} for downloading app from Google Play Store')
+
+                if not os.path.exists(path_to_file):
+                    os.mkdir(path_to_file)
+                    Log.info(
+                        f'Google Play - Creating directory {path_to_file} for downloading split app')
+
+                with open(f'{path_to_file}/{package_name}.apk', 'wb') as file:
+                    for chunk in downloaded_file.get('file').get('data'):
+                        file.write(chunk)
+
+                for split in downloaded_file['splits']:
+                    split_name = split['name']
+                    with open(f'{path_to_file}/{split_name}.apk', 'wb') as file:
+                        for chunk in split.get('file').get('data'):
+                            file.write(chunk)
+
+                if os.path.exists(path_to_file):
+                    Log.info('Google Play - Application with split successfully downloaded!')
+                else:
+                    Log.info('Google Play - Failed to download application. '
+                             'Seems like something is wrong with your file path or app file is broken')
+                    sys.exit(4)
 
         return path_to_file
 
