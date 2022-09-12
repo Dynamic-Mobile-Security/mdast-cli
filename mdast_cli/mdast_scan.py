918927d4e26919804dbc8e7c0ea8b40a5584e211
sergey <sergej.batishchev@gmail.com>
2022-09-12 12:53:14 +0300

    Set INFO default log level

--- mdast_cli/mdast_scan.py
+++ mdast_cli/mdast_scan.py
@@ -16,7 +16,7 @@ from helpers.helpers import check_app_md5
 
 from mdast_cli_core.token import mDastToken as mDast
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
 logger = logging.getLogger(__name__)
 
