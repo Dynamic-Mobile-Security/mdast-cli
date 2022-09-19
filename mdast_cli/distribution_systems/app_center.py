2d4956d1b7fa052c362e75b040014ac5c25e036b
sergey <sergej.batishchev@gmail.com>
2022-09-19 10:59:29 +0300

    Refactor distribution classes

--- mdast_cli/distribution_systems/app_center.py
+++ mdast_cli/distribution_systems/app_center.py
@@ -3,27 +3,28 @@ import os
 
 import requests
 
-from .base import DistributionSystem
-
 logger = logging.getLogger(__name__)
 
 
-class AppCenter(DistributionSystem):
+class AppCenter(object):
     """
     Downloading application from AppCenter distribution system
     """
     url = 'https://api.appcenter.ms/v0.1'
 
-    def __init__(self, token, app_name, owner_name, version, id):
-        super().__init__(app_name, version)
-
-        self.id = id
+    def __init__(self, token, owner_name):
         self.owner_name = owner_name
         self.auth_header = {'X-API-Token': token}
 
-    def get_version_info_by_id(self):
+    def get_version_info(self, app_identifier, app_version, app_id=None):
+        if app_id:
+            return self.get_version_info_by_id(app_identifier, app_id)
+
+        return self.get_version_info_by_version(app_identifier, app_id, app_version)
+
+    def get_version_info_by_id(self, app_identifier, app_id):
         logger.info('AppCenter - Get information about application')
-        url = f'{self.url}/apps/{self.owner_name}/{self.app_identifier}/releases/{self.id}'
+        url = f'{self.url}/apps/{self.owner_name}/{app_identifier}/releases/{app_id}'
         response = requests.get(url, headers=self.auth_header)
         if response.status_code != 200:
             raise RuntimeError(
@@ -33,8 +34,8 @@ class AppCenter(DistributionSystem):
         version_info = response.json()
         return version_info
 
-    def get_version_info_by_version(self):
-        url = f'{self.url}/apps/{self.owner_name}/{self.app_identifier}/releases?scope=tester'
+    def get_version_info_by_version(self, app_identifier, app_id, app_version):
+        url = f'{self.url}/apps/{self.owner_name}/{app_identifier}/releases?scope=tester'
         response = requests.get(url, headers=self.auth_header)
         if response.status_code != 200:
             raise RuntimeError(
@@ -43,21 +44,17 @@ class AppCenter(DistributionSystem):
 
         versions_info = response.json()
         for version in versions_info:
-            if version['version'] != self.app_version:
+            if version['version'] != app_version:
                 continue
 
             self.id = version['id']
-            version_info = self.get_version_info_by_id()
+            version_info = self.get_version_info_by_id(app_identifier, app_id)
             return version_info
 
         return None
 
-    def download_app(self, download_path):
-        if self.id:
-            version_info = self.get_version_info_by_id()
-        else:
-            version_info = self.get_version_info_by_version()
-
+    def download_app(self, download_path, app_identifier, app_version, app_id=None):
+        version_info = self.get_version_info(app_identifier, app_version, app_id)
         if not version_info:
             logger.error('AppCenter - Failed to get app version information.'
                          ' Verify that you set up arguments correctly and try again')
@@ -70,7 +67,7 @@ class AppCenter(DistributionSystem):
             raise RuntimeError(f'AppCenter - Failed to download application. '
                                f'Request return status code: {response.status_code}')
 
-        file_name = '{0}-{1}.apk'.format(self.app_identifier, version_info['version'])
+        file_name = '{0}-{1}.apk'.format(app_identifier, version_info['version'])
         path_to_save = os.path.join(download_path, file_name)
 
         if not os.path.exists(download_path):
--- mdast_cli/distribution_systems/nexus.py
+++ mdast_cli/distribution_systems/nexus.py
@@ -5,31 +5,24 @@ from base64 import b64encode
 import requests
 import urllib3
 
-from .base import DistributionSystem
-
 urllib3.disable_warnings()
 
 logger = logging.getLogger(__name__)
 
 
-class NexusRepository(DistributionSystem):
-    session = None
+class NexusRepository(object):
     nexus_url = None
 
-    def __init__(self, nexus_url, login, password, repo_name, group_id, artifact_id, version):
-        super().__init__(artifact_id, version)
-
+    def __init__(self, nexus_url, login, password):
         self.nexus_url = nexus_url if not nexus_url.endswith('/') else nexus_url[:-1]
         self.login = b64encode(str.encode(login))
         self.password = b64encode(str.encode(password))
-        self.repo_name = repo_name
-        self.group_id = group_id
-        self.artifact_id = artifact_id
-        self.version = version
-
-        self.connect()
+        self.session = None
 
     def connect(self):
+        if self.session:  # we already connected
+            return
+
         self.session = requests.session()
 
         api_url = self.nexus_url + '/service/rapture/session'
@@ -44,10 +37,10 @@ class NexusRepository(DistributionSystem):
         elif json_response.status_code == 500:
             logger.error("NexusRepo: Nexus Repo server error 500 during authentication")
 
-    def search_component(self):
-
-        search_url = f"{self.nexus_url}/service/rest/v1/search?repository={self.repo_name}&name={self.artifact_id}" \
-                     f"&version={self.version}&group={self.group_id}"
+    def search_component(self, repo_name, group_id, artifact_id, version):
+        self.connect()
+        search_url = f"{self.nexus_url}/service/rest/v1/search?repository={repo_name}&name={artifact_id}" \
+                     f"&version={version}&group={group_id}"
         json_response = self.session.get(search_url, verify=False)
         component_search_result = json_response.json().get('items', {})
         if component_search_result:
@@ -55,19 +48,20 @@ class NexusRepository(DistributionSystem):
             logger.info(f'NexusRepo: Successfully find component: {component_search_result}')
             return component_search_result[-1]
         else:
-            logger.info(f'NexusRepo: Unable to find component in repository - {self.repo_name}, '
-                        f'name - {self.artifact_id}, version - {self.version}&group_id={self.group_id}')
+            logger.info(f'NexusRepo: Unable to find component in repository - {repo_name}, '
+                        f'name - {artifact_id}, version - {version}&group_id={group_id}')
             return None
 
-    def download_app(self, download_path):
+    def download_app(self, download_path, repo_name, group_id, artifact_id, version):
+        self.connect()
         download_url = ''
         file_name = ''
-        component_search_result = self.search_component()
+        component_search_result = self.search_component(repo_name, group_id, artifact_id, version)
         for asset in component_search_result.get('assets', {}):
             if asset.get('contentType', '') in ('application/vnd.android.package-archive', 'application/x-itunes-ipa'):
                 download_url = asset.get('downloadUrl')
                 file_name = download_url.split('/')[-1] if download_url.split('/')[
-                                                                   -1] != '' else f'{self.group_id}-{self.version}.apk'
+                                                                   -1] != '' else f'{group_id}-{version}.apk'
                 break
         if not download_url:
             logger.error(f'NexusRepo: Unable to find download URL: {len(component_search_result)}')
